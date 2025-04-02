ebd3bc51c03dfdfe72955abb5a8cd6243dd6c3e6
Tarantaika-52 <sasha-kvasnikov@mail.ru>
2025-04-02 23:06:42 +0300

    feat

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,13 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { LoggerMiddleware, SecurityMiddleware } from './common/middlewares';
-import { PrismaModule } from './shared/prisma/prisma.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { ProfileModule } from './modules/profile/profile.module';
-import { UserModule } from './modules/user/user.module';
+import { PrismaModule } from './prisma';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    AuthModule,
-    ProfileModule,
     PrismaModule,
-    UserModule,
   ],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
-    consumer.apply(SecurityMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
